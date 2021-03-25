3d56cf6bfe5279eaa3ff7bd688201ba359952ba2
daniel <38928409+denielfer@users.noreply.github.com>
2021-03-25 20:22:20 -0300

    Update arquivo_de_testes_mutiplos.c

--- arquivo_de_testes_mutiplos.c
+++ arquivo_de_testes_mutiplos.c
@@ -93,35 +93,35 @@ int** multiplyMatASM(int** xMat, int** yMat, int dim){
             temp=0;
 //        printf("entrando no asm\n");
         __asm__ __volatile__(
-        "start_loop_1:\n"
-        "       cmp %[i], %[dim]\n"
-        "       jge end_loop1\n"
-        "       mov %[j],0\n"
-        "       mov %[x],[%[xMat]+%[lx]*8]\n"
-        "       mov %[r],[%[rMat]+%[lx]*8]\n"
-        "       inc %[lx]\n"
-        "       inc %[i]\n"
-        "       start_loop2:\n"
-        "               cmp %[j],%[dim]\n"
-        "               jge start_loop_1\n"
-        "               mov %[k],0\n"
-        "               mov %[temp],0\n"
-        "               mov %[ly],0\n"
-        "               mov [%[r]+%[j]*4],%[temp]\n"
-        "               start_loop3:\n"
-        "                       cmp %[k],%[dim]\n"
-        "                       jge end_loop2\n"
-        "                       mov %[y], [%[yMat]+%[ly]*8]\n"
-        "                       mov %[temp], [ %[y]+%[j]*4 ]\n"
-        "                       imul %[temp], [%[x]+%[k]*4]\n"
-        "                       inc %[ly]\n"
-        "                       inc %[k]\n"
-        "                       add [%[r]+%[j]*4], %[temp]\n"
-        "                       jmp start_loop3\n"
-        "               end_loop2:\n"
-        "                       inc %[j]\n"
-        "                       jmp start_loop2\n"
-        "end_loop1:"
+        "start_loop_1:\n" // definimos a branch de inicio do
+        "       cmp %[i], %[dim]\n" // comparamos o valor de i com dim
+        "       jge end_loop1\n" // se i for >= a dim pulamos para o final do loop1
+        "       mov %[j],0\n"// zeramos o valor do de j para o procimo loop
+        "       mov %[x],[%[xMat]+%[lx]*8]\n" // caregamos a referencia da linha de x que vamos mutiplicar pelas colunas de y na variavel "x"
+        "       mov %[r],[%[rMat]+%[lx]*8]\n" // caregamos a referencia da linha de r na qual os resultados serao escritos
+        "       inc %[lx]\n"//incrementamos o contador de linha de x, que consiste no mesmo contador de linha da resultante, lx segue os valores de lx
+        "       inc %[i]\n"// incrementador o i, varaivel do loop
+        "       start_loop2:\n"// delcaração da branch inical do loop 2
+        "               cmp %[j],%[dim]\n" // compara o valor de j com o valor da dimenção
+        "               jge start_loop_1\n"// se j for maior ou igual o loop2 é terminado e pulamos para o inicio do loop1
+        "               mov %[k],0\n"// movemos k para 0, k=0, para realizarmos os procimo loop
+        "               mov %[temp],0\n"// guardamos 0 em tempo pous mov nao estava aceitando valores imediatos
+        "               mov %[ly],0\n"// zeramos o contador de linhas de y, pois ly segue os valores de k
+        "               mov [%[r]+%[j]*4],%[temp]\n" // movemos o valor de temp, 0, para o elemento que sera o resultado dessa mutilicação, para limpar o lixo, e especificamente 0 pois é o neutro da soma
+        "               start_loop3:\n"// criamos a branch inicial do loop3
+        "                       cmp %[k],%[dim]\n"// comparamos o valor de k com dim
+        "                       jge end_loop2\n"// se o valor for maior ou igual terminamos o loop e vamos para o conjunto de instruçoes do final do loop2
+        "                       mov %[y], [%[yMat]+%[ly]*8]\n" // caregamos a linha que contem o elemento a ser usado na mutiplicação da matriz y
+        "                       mov %[temp], [ %[y]+%[j]*4 ]\n" // movemos o elemento q sera usado da linha de y para temp
+        "                       imul %[temp], [%[x]+%[k]*4]\n" // mutiplicamos o elemento que foi carregado em temp pelo elemento correspondente da matriz x
+        "                       inc %[ly]\n" // incrementamos a linha de y
+        "                       inc %[k]\n" // incrementamos k, contador do loop
+        "                       add [%[r]+%[j]*4], %[temp]\n"// somamos o resultado da mutiplicação na posição correspondente na matriz resultado 
+        "                       jmp start_loop3\n"// pulamos para o inicio do loop3 para repetirmos o processo ate sua condição verdadeira
+        "               end_loop2:\n"// declaramos a branch do final do loop2
+        "                       inc %[j]\n" // incrementamos j, isso é feito aqui pois j é usado no loop3 e caso essa incrementação fosse feito anteriormente a mutiplicação seria erada
+        "                       jmp start_loop2\n"// pulamos para o inicio do loop1
+        "end_loop1:"// branch para o final do loop1, que consiste no final da mutiplicação
         :[rMat] "+r" (multMat), [lx] "+r" (lx), [ly] "+r" (ly), [i] "+r" (i), [j] "+r" (j), [k] "+r" (k), [temp] "+r" (temp),
          [x] "+r" (x), [y] "+r" (y), [r] "+r" (r), [yMat] "+r" (yMat), [xMat] "+r" (xMat), [dim] "+r" (dim)
         );
